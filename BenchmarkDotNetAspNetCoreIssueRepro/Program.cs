ba163894d9d87ff6e051bc31342570b1adb1d92b
Mauro Servienti <mauro.servienti@gmail.com>
2018-06-26 16:10:45 +0200

    Benchmark

--- BenchmarkDotNetAspNetCoreIssueRepro/Program.cs
+++ BenchmarkDotNetAspNetCoreIssueRepro/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BenchmarkDotNetAspNetCoreIssueRepro
 {
@@ -6,7 +11,30 @@ namespace BenchmarkDotNetAspNetCoreIssueRepro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<Tests>();
+
+            Console.Read();
+        }
+    }
+
+    public class Tests
+    {
+        HttpClient httpClient = null;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            httpClient = new WebApplicationFactory<AspNetCoreApi.Startup>()
+                .CreateClient(new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+        }
+
+        [Benchmark]
+        public async Task Execute()
+        {
+            await httpClient.GetAsync("/api/SampleApi");
         }
     }
 }
